db53b75f5096685f308ac099dc5677b7da6be3f6
Raushan Sinha <sinharaushan@Raushans-MacBook-Pro.local>
2017-06-30 01:29:28 -0400

    Added resuable button with click event handler

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
 
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
 
     const {
         thumbnailStyle,
@@ -39,7 +39,9 @@ const AlbumDetail = ({ album }) => {
             </CardSection>
 
             <CardSection>
-                <Button />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                    </Button>
             </CardSection>
         </Card>
     );
@@ -47,8 +49,8 @@ const AlbumDetail = ({ album }) => {
 
 const styles = {
     thumbnailStyle: {
-       height: 50,
-       width: 50
+        height: 50,
+        width: 50
     },
     headerContentStyle: {
         flexDirection: 'column',
